db63646d96a48c2f8faad039a25ac0937ad8c15e
senya <etendren@gmail.com>
2018-02-15 11:34:39 +0300

    add battery to install.sh

--- .config/awesome/install.sh
+++ .config/awesome/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+git clone https://github.com/deficient/battery-widget.git
+
 git clone http://git.sysphere.org/awesome-configs ~/.config/awesome/awesome-configs
 cp -r ~/.config/awesome/awesome-configs/scratch ~/.config/awesome
 rm -rf ~/.config/awesome/awesome-configs
